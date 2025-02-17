914784dc576eb56168c6b2be692eb8f9c9bec092
idcpj <260083304@qq.com>
2025-02-17 22:15:57 +0800

    change local_notify

--- lib/core/platform/notification/notification_windows.dart
+++ lib/core/platform/notification/notification_windows.dart
@@ -1,44 +1,52 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter_local_notifications_windows/flutter_local_notifications_windows.dart';
+import 'package:local_notifier/local_notifier.dart';
 import '../../exceptions/initialize_exception.dart';
 import 'notification_interface.dart';
+import "package:local_notifier/local_notifier.dart";
+import 'package:package_info_plus/package_info_plus.dart';
 
 class NotificationWindows implements NotificationAbstract {
-  final _notifications = FlutterLocalNotificationsWindows();
-  bool _initialized = false;
-
   NotificationWindows() {
     initialize();
   }
 
   Future<void> initialize() async {
-    const settings = WindowsInitializationSettings(
-      appName: 'test',
-      appUserModelId: 'com.test.test',
-      guid: 'a8c22b55-049e-422f-b30f-863694de08c8',
+    final packageInfo = await PackageInfo.fromPlatform();
+    await localNotifier.setup(
+      appName: packageInfo.appName,
+      // 参数 shortcutPolicy 仅适用于 Windows
+      shortcutPolicy: ShortcutPolicy.requireCreate,
     );
-
-    final res = await _notifications.initialize(settings);
-    if (res == false) throw InitializeException('Windows通知初始化失败');
-
-    debugPrint('[notification] Windows通知初始化成功');
-    _initialized = true;
   }
 
   @override
   Future<void> show({required String title, required String body}) async {
-    if (!_initialized) {
-      throw InitializeException('[notification] 通知未初始化，请先调用 initialize()');
-    }
-
-    await _notifications.show(
-      1,
-      title,
-      body,
-      details: WindowsNotificationDetails(
-        subtitle: '',
-        images: [],
-      ),
+    LocalNotification notification = LocalNotification(
+      title: title,
+      body: body,
     );
+    notification.onShow = () {
+      print('onShow ${notification.identifier}');
+    };
+    notification.onClose = (closeReason) {
+      // 只支持在windows，其他平台 closeReason 始终为 unknown。
+      switch (closeReason) {
+        case LocalNotificationCloseReason.userCanceled:
+          // do something
+          break;
+        case LocalNotificationCloseReason.timedOut:
+          // do something
+          break;
+        default:
+      }
+      print('onClose  - $closeReason');
+    };
+    notification.onClick = () {
+      print('onClick ${notification.identifier}');
+    };
+    notification?.onClickAction = (actionIndex) {
+      print('onClickAction ${notification?.identifier} - $actionIndex');
+    };
+
+    notification.show();
   }
 }
